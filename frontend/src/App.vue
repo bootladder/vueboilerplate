f3c91a1662dda79a9236e54d2bdd004d7958ceb7
Steve Anderson <steve@bootladder.com>
2022-11-15 03:53:10 -0500

    add stuff

---
+++ frontend/src/App.vue
@@ -0,0 +1,50 @@
+<script>
+
+import {onMounted,ref, watch} from 'vue/dist/vue.esm-bundler.js';
+
+const TitleBar = {
+    template: '<div class="border border-black text-center font-bold bg-orange-300 rounded-lg"> Boilerplate </div>'
+}
+    
+export default {
+	components: {
+        TitleBar,
+	},
+	setup () {
+
+        const env = ref({
+            thedatamodel: { shapes: {}},
+        })
+        
+		return {
+            env: env,
+		}
+	},
+
+
+  watch: {
+    "env.thedatamodel": { async handler(newr, oldr) {
+            }, deep:true,
+        },
+      },
+
+	methods: {
+	},
+
+	computed: {
+	},
+
+
+  async mounted() {
+  }
+}
+
+</script>
+
+<template>
+
+    <div class="">
+        <TitleBar/>
+    </div>
+</template>
+
