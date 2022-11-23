de9e34aaff6ce6f50a7728271c7662f2d8a75dc0
Steve Anderson <steve@bootladder.com>
2022-11-23 13:05:46 -0500

    layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,15 +20,17 @@ export default {
         })
         
 		return {
-            env: env,
-      model: 3,
-      colors: [
-        'primary',
-        'secondary',
-        'yellow darken-2',
-        'red',
-        'orange',
-      ],
+          env: env,
+            selectedobject: ref({}),
+          model: 3,
+            pathlegs: ['Q','W','E','R','T'],
+          colors: [
+            'primary',
+            'secondary',
+            'yellow darken-2',
+            'red',
+            'orange',
+          ],
 		}
 	},
 
@@ -59,12 +61,59 @@ export default {
 
 </script>
 
+<style>
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@layer components {
+    .bb  {
+        @apply border border-black
+    }
+    .pathlegsetbutton{
+        @apply text-center text-xs bg-red-400;
+    }
+    .flexcol{ @apply flex flex-col }
+    .flexrow{ @apply flex flex-row }
+    .pathlegitem { @apply flexcol mx-1 px-1 bb}
+    .ctrlbutton{ @apply w-6 h-6 bg-red-400 rounded-xl text-center font-bold bb m-1 }
+}
+</style>
+
 <template>
 {{env}}
 
     <div class="bg-red-100">
         <TitleBar/>
         <v-btn>hello </v-btn >
+
+    <!-- Path Nav -->
+    <div class="flexrow my4">
+        <div v-for="(v,k) in pathlegs" 
+            class="pathlegitem">
+            <div class="ctrlbutton"> {{v}} </div>
+            <div class=""> 12345678 </div>
+        </div>
+    </div>
+
+    <!-- Obj Render -->
+    <div class="bg-blue-100 w-[400px] h-[200px]">
+        <div>
+            {{selectedobject}}
+        </div>
+    </div>
+
+    <!-- REPL -->
+    <div class="flexrow bg-yellow-200 w-[600px] h-12">
+        <div class="ctrlbutton"> A </div>
+        <div class="">
+            <div class="w-[300px] bb"> {{selectedobject}} </div>
+        </div>
+        <div class="ctrlbutton"> X </div>
+        <div class="ctrlbutton"> C </div>
+    </div>
+
     </div>
 </template>
 
