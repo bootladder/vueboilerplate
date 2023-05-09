aa8c93b6661f45a213bf4f14ec89cb1074edc623
Steve Anderson <steve@bootladder.com>
2023-05-09 03:00:48 -0400

    webshellinit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,8 +25,17 @@ export default {
 		return {
             ics: ref(initcommands),
             ics1: ref(initcommands1),
+            webshellinit: ref({}),
+            initready: ref(false),
 		}
 	},
+    mounted() {
+        fetch("/command", {
+            method: "POST",
+            body: "cat ~/.webshellinit.poop",
+        }).then( s => s.json()).then(s =>  {this.webshellinit = s;this.initready=true})
+        
+    },
 
 }
 
@@ -34,14 +43,38 @@ export default {
 
 
 <template>
-    <div class="text-xs">
+    <div v-if="initready" class="text-xs">
         <h1> Web Shell </h1>
+        <h1> Init: {{webshellinit}} </h1>
+        <pre> {{webshellinit}} </pre>
+        <pre> {{webshellinit.shellwindows}} </pre>
+        
+        <div class="flex flex-row">
+            <template v-for="(v,k) in webshellinit.framewindowlayout[0]" >
+                <div class="w-24 h-24 bg-red-100 border border-black">
+                    <OneShotCommand :command="webshellinit.shellwindows[v]" />
+                </div>
+            </template>
+        </div>
+        
+        <div class="flex flex-row">
+            <template v-for="(v,k) in webshellinit.framewindowlayout[1]" >
+                <div class="w-24 h-24 bg-red-100 border border-black">
+                </div>
+            </template>
+        </div>
+
+
+        <template v-for="(v,k) in webshellinit.shellwindows" >
+            <OneShotCommand :command="v" />
+        </template>
+    </div>
+</template>
+<!--
         <template v-for="(v,k) in ics1" >
             <OneShotCommand :command="v" />
         </template>
         <template v-for="(v,k) in ics" >
             <OneShotCommand :command="v" />
         </template>
-    </div>
-</template>
-
+-->
