182d85bff6537bd8b5abf61ee0e8166b988f4c8c
Steve Anderson <steve@bootladder.com>
2023-05-09 11:53:12 -0400

    style

--- frontend/src/AddNewNote.vue
+++ frontend/src/AddNewNote.vue
@@ -9,18 +9,18 @@ const savebuttonclicked = (s) => {
     fetch(url, {
         method:"POST",
         body:JSON.stringify({note: newnote.value}),
-    }).then(s => emits('dirty', {})).catch(e=>alert('fail fetch' + e))
+    }).then(s => {emits('dirty', {}); newnote.value=""})
+    .catch(e=>alert('fail fetch' + e))
 }
 </script>
 
 <template>
-    <div class="border border-black p-2 bg-gray-100 text-xs w-fit">
-        <div> Add New Note </div>
-        <textarea v-model="newnote" class=""/>
+    <div class="border border-black p-1 bg-gray-100 text-xs w-96">
+        <div class="mb-2"> Add New Note </div>
+        <textarea v-model="newnote" class="w-full h-48 p-1 m-1 border-gray border"/>
         <div class="border border-black rounded-md bg-blue-100 w-10 text-center"
                 @click="savebuttonclicked"> 
             Save 
         </div>
-        <div> the note is : {{newnote}} </div>
     </div>
 </template>
